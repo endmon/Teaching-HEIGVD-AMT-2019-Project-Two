d4c7690ff4378efe134243b905cda2a068e66551
Miguel Gouveia <miguel.lopesgouveia@heig-vd.ch>
2020-01-19 18:23:24 +0100

    Merge mysql containers

--- images/mysql/data/flight_manager.sql
+++ images/mysql/data/flight_manager.sql
@@ -50,3 +50,33 @@ CREATE TABLE flightReservation (
   FOREIGN KEY (customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   FOREIGN KEY (flight_id) REFERENCES flight (flight_id) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+--
+-- User manager DataBase
+--
+DROP SCHEMA IF EXISTS user_auth;
+CREATE SCHEMA user_auth;
+USE user_auth;
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE user (
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  email VARCHAR(90) NOT NULL UNIQUE,
+  password VARCHAR(1000) NOT NULL,
+  admin BOOLEAN NOT NULL,
+  PRIMARY KEY  (user_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/user_data.csv' INTO TABLE user_auth.user FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' (first_name, last_name, email, password, admin);
